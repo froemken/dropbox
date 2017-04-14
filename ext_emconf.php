2a0593d2057d7cf3bb48df4c801fbf7a202a8b74
Stefan Froemken <froemken@gmail.com>
2017-04-14 02:59:49 +0200

    No support for TYPO3 6.2 anymore. PHP version >= 5.4 is needed

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = array (
     'constraints' => array (
         'depends' => array (
             'php' => '5.4.0-7.99.99',
-            'typo3' => '6.2.0-8.99.99',
+            'typo3' => '7.6.0-8.99.99',
         ),
         'conflicts' => array (
         ),
