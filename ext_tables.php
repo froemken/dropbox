1ab4b63d2d61d96e38d4cf21428418429e7a438c
Stefan Froemken <froemken@gmail.com>
2019-03-07 22:07:05 +0100

    Update ext_localconf and ext_table

--- ext_tables.php
+++ ext_tables.php
@@ -4,9 +4,9 @@ if (!defined('TYPO3_MODE')) {
 }
 
 // Register accessToken wizard
-if (!\TYPO3\CMS\Core\Utility\GeneralUtility::compat_version('8.0')) {
+/*if (!\TYPO3\CMS\Core\Utility\GeneralUtility::compat_version('8.0')) {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModulePath(
         'access_token',
         'EXT:fal_dropbox/Classes/Wizards/AccessToken/'
     );
-}
+}*/
