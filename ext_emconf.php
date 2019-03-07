28bee22541cebd1e651b9c080288702cf6287133
Stefan Froemken <froemken@gmail.com>
2019-03-07 21:37:34 +0100

    Remove TYPO3 7 as minimal dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array (
     'version' => '2.0.0',
     'constraints' => array (
         'depends' => array (
-            'typo3' => '8.7.0-9.5.99',
+            'typo3' => '8.7.13-9.5.99',
         ),
         'conflicts' => array (
         ),
