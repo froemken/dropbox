cecd47a1ce75ad41b0fc44406bbb8b1f5d768acc
Stefan Froemken <froemken@gmail.com>
2020-05-16 21:07:47 +0200

    Remove old properties from ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,35 +1,17 @@
 <?php
 
-/*****************************************************************
- * Extension Manager/Repository config file for ext "fal_dropbox".
- *
- * Auto generated 17.07.2015 14:08
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- *****************************************************************/
-
 $EM_CONF[$_EXTKEY] = [
     'title' => 'FAL Dropbox',
     'description' => 'Provides a DropBox driver for TYPO3 File Abstraction Layer.',
-    'category' => 'plugin',
+    'category' => 'fe',
     'author' => 'Stefan Froemken',
     'author_email' => 'froemken@gmail.com',
-    'author_company' => 'Stefan Froemken',
-    'shy' => '',
-    'priority' => '',
-    'module' => '',
     'state' => 'stable',
-    'internal' => '',
-    'uploadfolder' => '0',
-    'createDirs' => '',
-    'clearCacheOnLoad' => 0,
-    'lockType' => '',
-    'version' => '3.0.2',
+    'clearCacheOnLoad' => true,
+    'version' => '4.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.13-9.5.99',
+            'typo3' => '10.4.0-10.4.99',
         ],
         'conflicts' => [
         ],
