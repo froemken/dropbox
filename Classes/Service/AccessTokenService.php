f011753999a5eeda66b2918f56894ad61b65143e
Stefan Froemken <froemken@gmail.com>
2020-05-16 21:07:10 +0200

    Replace TYPO3 with fal dropbox in doc header

--- Classes/Service/AccessTokenService.php
+++ Classes/Service/AccessTokenService.php
@@ -3,7 +3,7 @@ declare(strict_types = 1);
 namespace SFroemken\FalDropbox\Service;
 
 /*
- * This file is part of the TYPO3 CMS project.
+ * This file is part of the FAL Dropbox project.
  *
  * It is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License, either version 2
