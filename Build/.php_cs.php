f25f10da1d49cbecb1d82bc0eae199d5382bcfe1
Stefan Froemken <froemken@gmail.com>
2022-02-10 20:48:31 +0100

    Update to php-cs-fixer 3.6

--- Build/.php_cs.php
+++ Build/.php_cs.php
@@ -27,16 +27,14 @@ For the full copyright and license information, please read the
 LICENSE file that was distributed with this source code.
 COMMENT;
 
-// Return a Code Sniffing configuration using
-// all sniffers needed for PSR-2
-// and additionally:
-//  - Remove leading slashes in use clauses.
-//  - PHP single-line arrays should not have trailing comma.
-//  - Single-line whitespace before closing semicolon are prohibited.
-//  - Remove unused use statements in the PHP source code
-//  - Ensure Concatenation to have at least one whitespace around
-//  - Remove trailing whitespace at the end of blank lines.
-return PhpCsFixer\Config::create()
+$finder = PhpCsFixer\Finder::create()
+    ->name('*.php')
+    ->exclude('.build')
+    ->exclude('var')
+    ->in(__DIR__);
+
+$config = new PhpCsFixer\Config();
+return $config
     ->setRiskyAllowed(true)
     ->setRules([
         '@DoctrineAnnotation' => true,
@@ -53,7 +51,6 @@ return PhpCsFixer\Config::create()
         'declare_equal_normalize' => ['space' => 'none'],
         'dir_constant' => true,
         'function_typehint_space' => true,
-        'hash_to_slash_comment' => true,
         'lowercase_cast' => true,
         'method_argument_space' => ['on_multiline' => 'ensure_fully_multiline'],
         'modernize_types_casting' => true,
@@ -63,7 +60,7 @@ return PhpCsFixer\Config::create()
         'no_blank_lines_after_phpdoc' => true,
         'no_empty_phpdoc' => true,
         'no_empty_statement' => true,
-        'no_extra_consecutive_blank_lines' => true,
+        'no_extra_blank_lines' => true,
         'no_leading_import_slash' => true,
         'no_leading_namespace_whitespace' => true,
         'no_null_property_initialization' => true,
@@ -76,7 +73,7 @@ return PhpCsFixer\Config::create()
         'no_useless_else' => true,
         'no_whitespace_in_blank_line' => true,
         'ordered_imports' => true,
-        'php_unit_construct' => ['assertEquals', 'assertSame', 'assertNotEquals', 'assertNotSame'],
+        'php_unit_construct' => true,
         'php_unit_mock_short_will_return' => true,
         'php_unit_test_case_static_method_calls' => ['call_type' => 'self'],
         'phpdoc_no_access' => true,
@@ -87,14 +84,9 @@ return PhpCsFixer\Config::create()
         'phpdoc_types' => true,
         'phpdoc_types_order' => ['null_adjustment' => 'always_last', 'sort_algorithm' => 'none'],
         'return_type_declaration' => ['space_before' => 'none'],
+        'single_line_comment_style' => false,
         'single_quote' => true,
         'single_trait_insert_per_statement' => true,
         'whitespace_after_comma_in_array' => true
     ])
-    ->setFinder(
-        \PhpCsFixer\Finder::create()
-            ->name('*.php')
-            ->exclude('.build')
-            ->exclude('var')
-            ->in(__DIR__)
-    );
+    ->setFinder($finder);
