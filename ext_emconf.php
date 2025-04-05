60ef6622800cc72454fc1109924b7f1022436bb8
Stefan Froemken <froemken@gmail.com>
2025-04-05 14:21:57 +0200

    Add TYPO3 13 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,8 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '5.0.1',
     'constraints' => [
         'depends' => [
-            'php' => '7.4.0-8.2.99',
-            'typo3' => '11.5.30-12.4.99',
+            'typo3' => '12.4.25-13.4.99',
         ],
         'conflicts' => [
         ],
