3b6e09927b5db171a5f0f4cc1de0f74c57b9a0f1
Stefan Froemken <froemken@gmail.com>
2025-05-28 19:44:10 +0200

    Use short classnames in ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,49 +4,53 @@ if (!defined('TYPO3')) {
     die('Access denied.');
 }
 
-call_user_func(static function (): void {
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['registeredDrivers']['dropbox'] = [
-        'class' => \StefanFroemken\Dropbox\Driver\DropboxDriver::class,
-        'shortName' => 'Dropbox',
-        'flexFormDS' => 'FILE:EXT:dropbox/Configuration/FlexForms/Dropbox.xml',
-        'label' => 'Dropbox',
-    ];
+use StefanFroemken\Dropbox\Driver\DropboxDriver;
+use StefanFroemken\Dropbox\Extractor\ImageExtractor;
+use StefanFroemken\Dropbox\Form\Element\DropboxStatusElement;
+use StefanFroemken\Dropbox\Form\Element\RefreshTokenElement;
+use StefanFroemken\Dropbox\Resource\Processing\ImageProcessing;
+use StefanFroemken\Dropbox\Upgrade\RenameExtensionKeyUpgrade;
+use TYPO3\CMS\Core\Cache\Backend\TransientMemoryBackend;
+use TYPO3\CMS\Core\Resource\Index\ExtractorRegistry;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-    // Create a temporary cache
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['dropbox']['backend']
-        = \TYPO3\CMS\Core\Cache\Backend\TransientMemoryBackend::class;
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['registeredDrivers']['dropbox'] = [
+    'class' => DropboxDriver::class,
+    'shortName' => 'Dropbox',
+    'flexFormDS' => 'FILE:EXT:dropbox/Configuration/FlexForms/Dropbox.xml',
+    'label' => 'Dropbox',
+];
 
-    // Add wizard/control to access_token in XML structure
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1552228283] = [
-        'nodeName' => 'refreshToken',
-        'priority' => '70',
-        'class' => \StefanFroemken\Dropbox\Form\Element\RefreshTokenElement::class,
-    ];
-    // Show dropbox status in file storage
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1589660489] = [
-        'nodeName' => 'dropboxStatus',
-        'priority' => '70',
-        'class' => \StefanFroemken\Dropbox\Form\Element\DropboxStatusElement::class,
-    ];
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['dropbox']['backend'] = TransientMemoryBackend::class;
 
-    // Image Extractor is used to get image dimension from Dropbox API
-    \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-        \TYPO3\CMS\Core\Resource\Index\ExtractorRegistry::class
-    )->registerExtractionService(\StefanFroemken\Dropbox\Extractor\ImageExtractor::class);
+// Add wizard/control to access_token in XML structure
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1552228283] = [
+    'nodeName' => 'refreshToken',
+    'priority' => '70',
+    'class' => RefreshTokenElement::class,
+];
+// Show dropbox status in file storage
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1589660489] = [
+    'nodeName' => 'dropboxStatus',
+    'priority' => '70',
+    'class' => DropboxStatusElement::class,
+];
 
-    // Register processor to resize images directly in dropbox and download just the thumbnail
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['processors']['dropboxImagePreviewProcessor'] = [
-        'className' => \StefanFroemken\Dropbox\Resource\Processing\ImageProcessing::class,
-        'after' => [
-            'SvgImageProcessor',
-        ],
-        'before' => [
-            'LocalImageProcessor',
-            'OnlineMediaPreviewProcessor',
-            'DeferredBackendImageProcessor',
-        ],
-    ];
+// Image Extractor is used to get image dimension from Dropbox API
+GeneralUtility::makeInstance(ExtractorRegistry::class)
+    ->registerExtractionService(ImageExtractor::class);
 
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['DropboxRenameExtensionKey']
-        = \StefanFroemken\Dropbox\Upgrade\RenameExtensionKeyUpgrade::class;
-});
+// Register processor to resize images directly in dropbox and download just the thumbnail
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['processors']['dropboxImagePreviewProcessor'] = [
+    'className' => ImageProcessing::class,
+    'after' => [
+        'SvgImageProcessor',
+    ],
+    'before' => [
+        'LocalImageProcessor',
+        'OnlineMediaPreviewProcessor',
+        'DeferredBackendImageProcessor',
+    ],
+];
+
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['DropboxRenameExtensionKey'] = RenameExtensionKeyUpgrade::class;
