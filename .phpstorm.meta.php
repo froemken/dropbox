2d2f4a232d562605fd38c0653b425267c81ed6f7
Stefan Froemken <froemken@gmail.com>
2026-05-14 20:23:38 +0200

    [BUGFIX] Adjust deprecated PreviewAspect references
    Replaced deprecated `Context\PreviewAspect` namespace with the updated
    `Aspect\PreviewAspect` in PHPStorm metadata. Added missing frontend-specific
    context attributes for improved auto-completion and type-hinting support.

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -25,7 +25,7 @@ namespace PHPSTORM_META {
         'frontend.user' => \TYPO3\CMS\Core\Context\UserAspect::class,
         'workspace' => \TYPO3\CMS\Core\Context\WorkspaceAspect::class,
         'language' => \TYPO3\CMS\Core\Context\LanguageAspect::class,
-        'frontend.preview' => \TYPO3\CMS\Frontend\Context\PreviewAspect::class,
+        'frontend.preview' => \TYPO3\CMS\Frontend\Aspect\PreviewAspect::class,
     ]));
     expectedArguments(
         \TYPO3\CMS\Core\Context\DateTimeAspect::get(),
@@ -71,7 +71,7 @@ namespace PHPSTORM_META {
         'legacyOverlayType'
     );
     expectedArguments(
-        \TYPO3\CMS\Frontend\Context\PreviewAspect::get(),
+        \TYPO3\CMS\Frontend\Aspect\PreviewAspect::get(),
         0,
         'isPreview'
     );
@@ -86,11 +86,13 @@ namespace PHPSTORM_META {
         'routing',
         'module',
         'moduleData',
-        'frontend.controller',
         'frontend.typoscript',
         'frontend.cache.collector',
         'frontend.cache.instruction',
         'frontend.page.information',
+        'frontent.page.parts',
+        'frontend.register.stack',
+        'frontend.response.data'
     );
     override(\Psr\Http\Message\ServerRequestInterface::getAttribute(), map([
         'frontend.user' => \TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication::class,
@@ -100,11 +102,13 @@ namespace PHPSTORM_META {
         'routing' => '\TYPO3\CMS\Core\Routing\SiteRouteResult|\TYPO3\CMS\Core\Routing\PageArguments',
         'module' => \TYPO3\CMS\Backend\Module\ModuleInterface::class,
         'moduleData' => \TYPO3\CMS\Backend\Module\ModuleData::class,
-        'frontend.controller' => \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController::class,
         'frontend.typoscript' => \TYPO3\CMS\Core\TypoScript\FrontendTypoScript::class,
         'frontend.cache.collector' => \TYPO3\CMS\Core\Cache\CacheDataCollector::class,
         'frontend.cache.instruction' => \TYPO3\CMS\Frontend\Cache\CacheInstruction::class,
         'frontend.page.information' => \TYPO3\CMS\Frontend\Page\PageInformation::class,
+        'frontend.page.parts' => \TYPO3\CMS\Frontend\Page\PageParts::class,
+        'frontend.register.stack' => \TYPO3\CMS\Frontend\ContentObject\RegisterStack::class,
+        'frontend.response.data' => \TYPO3\CMS\Frontend\Response\ResponseData::class,
     ]));
 
     expectedArguments(
@@ -145,9 +149,5 @@ namespace PHPSTORM_META {
 
     override(\Psr\EventDispatcher\EventDispatcherInterface::dispatch(0), map([
         '' => '@',
-    ]));
-
-    override(\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(0), map([
-        '' => '@'
     ]));
 }
